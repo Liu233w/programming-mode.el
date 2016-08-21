eb36f652f457168370aa3d3d751b3b3c68e837a5
Liu's DeskTop <wwwlsmcom@msn.cn>
2016-08-21 16:45:22 +0800

    use quail to translate keys

--- programming-mode.el
+++ programming-mode.el
@@ -1,98 +1,55 @@
-;;; programming-mode.el
+;;; programming-mode.el -- Input method to exchange your number line with the mark above it.
+;;
+;; Copyright © 2016 Liu233w
+;;
+;; Author: Liu233w <wwwlsmcom@outlook.com>
+;; URL: https://github.com/Liu233w/programming-mode.el
+;; Version: 0.1.0
+;; Keywords: input-method keyboard
 
-;;; author: Liu233w
+;; This file is not part of GNU Emacs.
 
-;;; Copyright (c) 2016 Liu233w. All rights reserved.
-;;; Work in progress,not completed.
+;;; Commentary:
 
-(defconst programming-mode--key-translation
-  '(
-    ("1" . "!")
-    ("2" . "@")
-    ("3" . "#")
-    ("4" . "$")
-    ("5" . "%")
-    ("6" . "^")
-    ("7" . "&")
-    ("8" . "*")
-    ("9" . "(")
-    ("0" . ")")
-    )
-  "Define which keys to translate.")
+;; It's a input method to exchange your number line with the mark above it.
+;; For example, when you press `1', `!' will be entered.
+;; Pressing `S+1' will enter `1', etc.
+;; It's a input method, so it's only worked on insert mode in current buffer,
+;; so chords like `C-x 2' will act as normal, and the key won't be translated
+;; in minibuffer.
 
-(defvar after-programming-mode-hook
-  nil
-  "a hook which will run after minor mode is started")
+;;; License: GPLv3
 
-(define-minor-mode programming-mode
-  "A minor mode which swap the key in your number line with the symbol above"
-  nil
-  "EP"
-  nil
-  :after-hook after-programming-mode-hook
-  )
+;;; Code:
+(require 'quail)
 
-;;; from https://www.emacswiki.org/emacs/Evil
-(defun make-conditional-key-translation (key-from key-to translate-keys-p)
-  "Make a Key Translation such that if the translate-keys-p function returns true,
- key-from translates to key-to, else key-from translates to itself.  translate-keys-p
- takes key-from as an argument. "
-  (define-key key-translation-map key-from
-    `(lambda (prompt)
-       (if (funcall (quote ,translate-keys-p) ,key-from) ,key-to ,key-from))))
+(quail-define-package
+ "programming-mode" "English" "&" t
+ "Press `1' to enter `!',
+Press `S+1' to enter `1' instead, etc."
+ nil t t t t nil nil nil nil nil t)
 
-(defvar programming-mode--translation-condition
-  '(and
-    programming-mode)
-  "A form to test if it should translate keys. It shouldn't be edited by hand,
-but should be edited by function `programming-mode-add-translation-condition'")
+(quail-define-rules
+ ("1" ?!)
+ ("2" ?@)
+ ("3" ?#)
+ ("4" ?$)
+ ("5" ?%)
+ ("6" ?^)
+ ("7" ?&)
+ ("8" ?*)
+ ("9" ?\()
+ ("0" ?\))
+ ("!" ?1)
+ ("@" ?2)
+ ("#" ?3)
+ ("$" ?4)
+ ("%" ?5)
+ ("^" ?6)
+ ("&" ?7)
+ ("*" ?8)
+ ("(" ?9)
+ (")" ?0))
 
-(defun programming-mode--refresh-translation-condition ()
-  "Redefine a function `programming-mode--translate-keys-p' to test if it should
-remap keys. The function is defined by the form in `programming-mode--translation-condition'."
-  (eval
-   `(defun programming-mode--translate-key-p (key-from)
-      "Returns whether conditional key translations should be active."
-      ,programming-mode--translation-condition)))
-
-(defun programming-mode-add-translation-condition (forms)
-  "Argument is a list of forms to test whether conditional key translations
-should be active. The function will add them to `programming-mode--translation-condition'
-and then call `programming-mode--refresh-translation-condition' to redefine
-`programming-mode--translate-key-p'.
-Variable `key-form' means the key you press to call `programming-mode--translate-key-p'.
-
-Example:
-(programming-mode-add-translation-condition
- (evil-insert-state-p))
-It will test if insert state (defined by evil) is actived.
-
-Example2:
-(programming-mode-add-translation-condition
- (not (evil-normal-state-p)))
-Add a condition that not in normal state."
-(setq programming-mode--refresh-translation-condition
-      (nconc programming-mode--translation-condition forms))
-(programming-mode--refresh-translation-condition))
-
-(defun programming-mode-add-key-translation (from to)
-  "Add a Key Translation to `key-translation-map'"
-  (make-conditional-key-translation from to 'programming-mode--translate-key-p))
-
-(defun programming-mode-setup ()
-  "Setup programming-mode by add the keys defined in `programming-mode--key-translation'
-into `key-translation-map'. Before that, refresh the predicate function
-`programming-mode--translate-key-p'."
-  (interactive)
-  (programming-mode--refresh-translation-condition)
-  (dolist (i programming-mode--key-translation)
-    (programming-mode-add-key-translation (car i) (cdr i))
-    (programming-mode-add-key-translation (cdr i) (car i))))
-
-(defun programming-mode-setup-evil-mode ()
-  "Setup key mapping for evil. Will call `programming-mode-setup' last."
-  (interactive)
-  (programming-mode-add-translation-condition
-   `((evil-insert-state-p)))
-  (programming-mode-setup)
-  )
+(provide 'programming-mode)
+;;; programming-mode.el ends here
